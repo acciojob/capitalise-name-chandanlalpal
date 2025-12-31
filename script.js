6c163fc3dc8f1d3fa3860f4a014f5de4071a03e3
chandan lal <148762955+chandanlalpal@users.noreply.github.com>
2025-12-31 07:55:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
  let input = document.querySelector("#fname");
 
-input.addEventListener("blur",(e)=>{
-  console.log(e.target.value.toUpperCase());
+input.addEventListener("blur",(e)=>{//name
+  // console.log(e.target.value.toUpperCase());
+
+	e.target.value=e.target.value.toUppderCase());
 })
